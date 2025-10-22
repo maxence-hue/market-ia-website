495e8893a3adc251d470870e0e22ea6c29a89daa
maxence-hue <maxence@adsolar.fr>
2025-10-22 17:07:24 +0200

    Fix Sanity: Add @sanity/code-input plugin for code blocks

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
+import { codeInput } from '@sanity/code-input'
 import { schema } from './sanity/schema'
 
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
 
   basePath: '/studio',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), codeInput()],
 
   schema,
 })
